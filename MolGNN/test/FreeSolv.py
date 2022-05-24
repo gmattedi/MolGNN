8dfe002c6f671948d86f8cc5451ae48969d4a04a
gmattedi <giulio.mattedi@gmail.com>
2022-05-24 11:54:13 +0100

    add regression test

--- MolGNN/test/FreeSolv.py
+++ MolGNN/test/FreeSolv.py
@@ -14,15 +14,15 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 CHECKPOINT_PATH = 'train/'
 # ------------------------
 
+# ------------- CLASSIFICATION ------------------------
 # Load the data and process in into PyG Data objects
 solv = pd.read_csv('FreeSolv.tsv', sep=';')
 smiles = solv.SMILES.values
 y = (
         solv['experimental value (kcal/mol)'].values <= solv['experimental value (kcal/mol)'].median()
 ).astype(int)
-data_lst = MolToGraph.create_pyg_data_lst(smiles, y=y)
 
-train_idx, val_idx = train_test_split(range(len(smiles)), test_size=0.2, random_state=123)
+train_idx, val_idx = train_test_split(range(len(smiles)), test_size=0.2, random_state=42)
 
 smiles_train, smiles_val = smiles[train_idx], smiles[val_idx]
 y_train, y_val = y[train_idx], y[val_idx]
@@ -36,7 +36,7 @@ val_loader = DataLoader(
     batch_size=32
 )
 
-model, result = models.train.train_graph_classifier(
+_, result = models.train.train_graph_classifier(
     train_loader=train_loader, val_loader=val_loader,
     num_node_features=79, num_classes=2,
     model_name="GraphConv",
@@ -50,3 +50,33 @@ model, result = models.train.train_graph_classifier(
 )
 
 print(result)
+
+# ------------- REGRESSION ------------------------
+y = solv['experimental value (kcal/mol)'].values
+
+train_idx, val_idx = train_test_split(range(len(smiles)), test_size=0.2, random_state=42)
+
+smiles_train, smiles_val = smiles[train_idx], smiles[val_idx]
+y_train, y_val = y[train_idx], y[val_idx]
+
+train_loader = DataLoader(
+    dataset=MolToGraph.create_pyg_data_lst(smiles_train, y_train),
+    batch_size=32
+)
+val_loader = DataLoader(
+    dataset=MolToGraph.create_pyg_data_lst(smiles_val, y_val),
+    batch_size=32
+)
+
+_, result = models.train.train_graph_regressor(
+    train_loader=train_loader, val_loader=val_loader,
+    num_node_features=79, num_classes=2,
+    model_name="GraphConv",
+    c_hidden=256,
+    layer_name="GraphConv",
+    num_layers=3,
+    dp_rate_linear=0.5,
+    dp_rate=0.0,
+    device=device,
+    checkpoint_path='./trained_model/'
+)
