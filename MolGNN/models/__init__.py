50df5a43a63e82fbc92e1bba1d3a8948458719b0
gmattedi <giulio.mattedi@gmail.com>
2022-05-24 11:49:37 +0100

    expose GraphClassifier and GraphRegressor

--- MolGNN/models/__init__.py
+++ MolGNN/models/__init__.py
@@ -1,4 +1,4 @@
 from MolGNN.models import train
 from MolGNN.models.GNNModel import GNNModel
 from MolGNN.models.GraphGNNModel import GraphGNNModel
-from MolGNN.models.GraphLevelGNN import GraphLevelGNN
+from MolGNN.models.GraphLevelGNN import GraphClassifier, GraphRegressor
